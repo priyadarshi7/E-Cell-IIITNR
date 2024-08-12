9347ebd3c77a661b46a0e43ac3638ab239fd2790
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-08-12 16:23:47 +0530

    init

---
+++ src/Components/Home/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './About.css'
+import Cover_Image from '../../../assets/Images/Cover_Images/About-Cover-Image.jpeg'
+
+const About = () => {
+  return (
+    <div className="about">
+        <div className="box">
+            <div className="sub-box">
+                <h1>WHO ARE WE ?</h1>
+                <img src={Cover_Image} alt="" />
+            </div>
+            <h3>E-Cell IIIT Naya Raipur took its start back in 2015 and has been growing and heightening ever since. We are here to foster and encourage all the entrepreneurship enthusiasts by providing them dynamic, informative and fun experiences through events and contests.</h3>
+            <button>Read More</button>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import About from '../../Components/Home/About/About'
+
+const Home = () => {
+  return (
+    <div className="home">
+        <About/>
+    </div>
+  )
+}
+
+export default Home
