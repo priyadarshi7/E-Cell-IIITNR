cdaea6ba0b3f8caad44d70352b54004a2ddbb33d
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-08-13 16:11:33 +0530

    main-page-init

--- src/Components/Home/Main/Main.jsx
+++ src/Components/Home/Main/Main.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import './Main.css'
+import React from 'react';
+import './Main.css';
 
 const Main = () => {
   return (
     <div className="main-content">
-        <div className="main-box">
-            <div className="main-text">
-                <div className="main-head"><h1>E-CELL</h1></div>
-                <div className="main-sub-text">DARE TO <span>DREAM</span>,</div>
-                <div className="main-sub-text">VENTURE TO <span>DREAM</span>,</div>
+      <div className="main-box">
+        <div className="main-text">
+          <div className="main-head">
+            <h1>E-CELL</h1>
+            <div className="sub-text">
+              <div className="main-sub-text">DARE TO <span>DREAM</span>,</div>
+              <div className="main-sub-text">VENTURE TO <span>SUCCEED</span>,</div>
+              <div className="text">
+                <div>Welcome to the Entrepreneur Cell of IIIT Naya Raipur,</div>
+                <div>where dreams take flight and ventures succeed. Join</div>
+                <div>us in shaping tomorrow's innovations!</div>
+              </div>
             </div>
-            <div className="main-logo"></div>
+          </div>
         </div>
+        <div className="main-logo"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
