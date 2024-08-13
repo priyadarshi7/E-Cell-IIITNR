e0147026f39abaf7190ee3c336462804dd67cf69
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-08-13 14:28:14 +0530

    main

---
+++ src/Components/Home/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+  return (
+    <div className="main-content">
+        <div className="main-box">
+            <div className="main-text">
+                <div className="main-head"><h1>E-CELL</h1></div>
+                <div className="main-sub-text">DARE TO <span>DREAM</span>,</div>
+                <div className="main-sub-text">VENTURE TO <span>DREAM</span>,</div>
+            </div>
+            <div className="main-logo"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
