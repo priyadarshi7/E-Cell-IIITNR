6fe78d12e815362bc54cf23c276801f02f6932b1
alisha3693 <tripathialisha03@gmail.com>
2024-08-18 01:58:18 +0530

    made gallery

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import About from '../../Components/Home/About/About'
 import Main from '../../Components/Home/Main/Main'
 import OurEvents from '../../Components/Our Events/OurEvents'
-
+import Gallery from '../../Components/Home/Gallery/Gallery'
 
 const Home = (props) => {
   return (
@@ -11,8 +11,11 @@ const Home = (props) => {
         <Main/>
         {props.loading===false && <div>
         <About/>
-        <OurEvents/>
-        </div>}
+        <Gallery/>
+        <OurEvents/>        
+        </div>
+         }
+      
     </div>
   )
 }
---
+++ src/assets/Data/GalleryImages.jsx
@@ -0,0 +1,7 @@
+export default [
+    'src/assets/Images/ecellGALLERY/0f4be012-d4e2-481a-b7a4-385b34e83234.jpeg',
+    'src/assets/Images/ecellGALLERY/6de8e3e9-aa9d-4e9e-b613-6d752bd00318.jpeg',
+    'src/assets/Images/ecellGALLERY/141dee6c-d8a9-4568-a57c-a49b29f7f650.jpeg',
+    'src/assets/Images/ecellGALLERY/43147b9f-c9c1-40a8-b599-34305f4ccbb4.jpeg',
+    'src/assets/Images/ecellGALLERY/fcf12961-0d8e-46bb-a220-8b95850a4432.jpeg',
+  ]
