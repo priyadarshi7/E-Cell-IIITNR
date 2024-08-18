a812901a6053891a2286f20d95336fec1e44ab04
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-08-18 16:39:05 +0530

    load

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -2,12 +2,24 @@ import React from "react"
 import Home from "../Home/Home"
 import TeamHome from "../../Components/Team/Team_Home/TeamHome"
 import TeamMain from "../../Components/Team/TeamMain/TeamMain"
+import Load from "../../Components/Load_Page/Load"
 
 export default function Team(){
+
+    let [load,setLoad] = React.useState(true);
+    React.useEffect(()=>{
+        setTimeout(()=>{
+            setLoad(false);
+        },4000)
+    },[load]);
     return(
         <div className="team">
+            {load===true && <Load/>}
+            <div>
             <TeamHome/>
-            <TeamMain/>
+
+            {load===false && <TeamMain/>}
+            </div>
         </div>
     )
 }
